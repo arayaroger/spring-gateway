0b02491ad3f3aa50a1e8f639cc6af9d6f269c122
Roger Araya <roger.araya@pfcti.com>
2023-05-03 19:38:17 -0600

    Commit clase 3-5-23 con ruteo en yml y cir uit break

--- src/main/java/com/demo/springgateway/routes/RoutesConfig.java
+++ src/main/java/com/demo/springgateway/routes/RoutesConfig.java
@@ -1,13 +1,17 @@
 package com.demo.springgateway.routes;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
-//@Configuration se comenta para configurar rutas por YML
+import java.util.Objects;
+
+@Configuration //se comenta para configurar rutas por YML
 public class RoutesConfig {
 
     @Value("${uri.api.clientes}")
@@ -16,25 +20,25 @@ public class RoutesConfig {
     @Value("${uri.api.cuentas}")
     private  String cuentasUri;
 
-    @Bean
-    public RouteLocator rutasDeClientes(RouteLocatorBuilder routeLocatorBuilder){
-
-        return routeLocatorBuilder
-                .routes()
-                .route("all_clientes", r -> r.path("/v1/api/cliente/all")
-                .filters(f -> f.addRequestHeader("X-GATEWAY-REQUEST-HEADER", "GATEWAY SOLICITUD")
-                        .addResponseHeader("X-GATEWAY-REQUEST-HEADER", "GATEWAY SOLICITUD"))
-                        .uri(clientesUri)
-                )
-                .route("cliente_id", r -> r.path("/v1/api/cliente/{id}")
-                        .and().method(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT)
-                        .filters(f -> f.addRequestHeader("X-GATEWAY-REQUEST-HEADER", "GATEWAY SOLICITUD")
-                                .addResponseHeader("X-GATEWAY-REQUEST-HEADER", "GATEWAY SOLICITUD"))
-                        .uri(clientesUri)
-                )
-                .build();
-
-    }
+//    @Bean
+//    public RouteLocator rutasDeClientes(RouteLocatorBuilder routeLocatorBuilder){
+//
+//        return routeLocatorBuilder
+//                .routes()
+//                .route("all_clientes", r -> r.path("/v1/api/cliente/all")
+//                .filters(f -> f.addRequestHeader("X-GATEWAY-REQUEST-HEADER", "GATEWAY SOLICITUD")
+//                        .addResponseHeader("X-GATEWAY-REQUEST-HEADER", "GATEWAY SOLICITUD"))
+//                        .uri(clientesUri)
+//                )
+//                .route("cliente_id", r -> r.path("/v1/api/cliente/{id}")
+//                        .and().method(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT)
+//                        .filters(f -> f.addRequestHeader("X-GATEWAY-REQUEST-HEADER", "GATEWAY SOLICITUD")
+//                                .addResponseHeader("X-GATEWAY-REQUEST-HEADER", "GATEWAY SOLICITUD"))
+//                        .uri(clientesUri)
+//                )
+//                .build();
+//
+//    }
 //OTRA FORMA DE ENMASCARAR URI
 //    public RouteLocator rutasDeClientes(RouteLocatorBuilder builder) {
 //        return builder.routes()
@@ -54,18 +58,22 @@ public class RoutesConfig {
 //                .build();
 //    }
 
+//    @Bean
+//    public RouteLocator rutasDeCuentas(RouteLocatorBuilder routeLocatorBuilder){
+//
+//        return routeLocatorBuilder
+//                .routes()
+//                .route("all_cuentas", r -> r.path("/api/v2/pokemon/ditto")
+//                        .filters(f -> f.addRequestHeader("X-GATEWAY-REQUEST-HEADER", "GATEWAY SOLICITUD")
+//                                .addResponseHeader("X-GATEWAY-REQUEST-HEADER", "GATEWAY SOLICITUD"))
+//                        .uri(cuentasUri)
+//                )
+//                .build();
+//
+//    }
     @Bean
-    public RouteLocator rutasDeCuentas(RouteLocatorBuilder routeLocatorBuilder){
-
-        return routeLocatorBuilder
-                .routes()
-                .route("all_cuentas", r -> r.path("/api/v2/pokemon/ditto")
-                        .filters(f -> f.addRequestHeader("X-GATEWAY-REQUEST-HEADER", "GATEWAY SOLICITUD")
-                                .addResponseHeader("X-GATEWAY-REQUEST-HEADER", "GATEWAY SOLICITUD"))
-                        .uri(cuentasUri)
-                )
-                .build();
-
+    public KeyResolver userKeyResolver(){
+        return exchange -> Mono.just(Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getAddress().getHostAddress());
     }
 
 }
